77ce78bfb5da1ed60c387d14099a65e3ec4c3801
nakon <nakonechndima@gmail.com>
2024-12-15 19:11:10 +0100

    Complete Snake Game

--- GameStateTest.py
+++ GameStateTest.py
@@ -1,52 +1,52 @@
 import unittest
-
-from scipy.special import expected
-
 import SnakeGame
 
 
 class MyTestCase(unittest.TestCase):
-    def snake_should_move_right(self):
+    def test_snake_should_move_right(self):
         state = SnakeGame.GameState(
             snake=[SnakeGame.Position(0, 0), SnakeGame.Position(0, 1), SnakeGame.Position(0, 2)],
             direction = SnakeGame.Direction.RIGHT,
             food = SnakeGame.Position(10,10),
             field_size=20
         )
-        state.step()
+        state.step_move_snake()
         expected = [SnakeGame.Position(0,1), SnakeGame.Position(0,2), SnakeGame.Position(0,3)]
         self.assertEqual(expected, state.snake)
-    def snake_should_move_left(self):
+
+    def test_snake_should_move_left(self):
         state = SnakeGame.GameState(
             snake=[SnakeGame.Position(1, 2), SnakeGame.Position(1, 3), SnakeGame.Position(1, 4)],
-            direction = SnakeGame.Direction.RIGHT,
+            direction = SnakeGame.Direction.LEFT,
             food = SnakeGame.Position(10,10),
             field_size=20
         )
-        state.step()
+        state.step_move_snake()
         expected = [SnakeGame.Position(1,3), SnakeGame.Position(1,4), SnakeGame.Position(0,4)]
         self.assertEqual(expected, state.snake)
-    def snake_should_move_up(self):
+
+    def test_snake_should_move_up(self):
         state = SnakeGame.GameState(
             snake =[SnakeGame.Position(1,2), SnakeGame.Position(1,3), SnakeGame.Position(1,4)],
             direction = SnakeGame.Direction.UP,
             food = SnakeGame.Position(10,10),
             field_size=20
         )
-        state.step()
+        state.step_move_snake()
         expected = [SnakeGame.Position(1,1), SnakeGame.Position(1,2), SnakeGame.Position(1,3)]
         self.assertEqual(expected, state.snake)
 
-    def snake_should_move_down(self):
+    def test_snake_should_move_down(self):
         state = SnakeGame.GameState(
             snake =[SnakeGame.Position(1,2), SnakeGame.Position(1,3), SnakeGame.Position(1,4)],
             direction = SnakeGame.Direction.DOWN,
             food = SnakeGame.Position(10,10),
             field_size=20
         )
-        state.step()
+        state.step_move_snake()
         expected = [SnakeGame.Position(1,3), SnakeGame.Position(1,4), SnakeGame.Position(1,5)]
         self.assertEqual(expected, state.snake)
 
-if __name__ == '__main__':
-    unittest.main()
+# class ExampleTest(unittest.TestCase):
+#     def test_example(self):
+#         self.assertEqual('foo'.upper(), 'FOO')
--- SnakeGame.py
+++ SnakeGame.py
@@ -1,13 +1,20 @@
 #That code's game provided by book writen by Marcin Moskala 12.12.2024
 import pygame
+from random import randint
 pygame.init()
 cube_size = 25
 cubes_num = 20
 width = cube_size * cubes_num
 screen = pygame.display.set_mode((width, width))
-pygame.display.set_caption("Snale Game")
+pygame.display.set_caption("Snake Game")
 screen.fill((255, 255, 255))
 pygame.display.update()
+
+class Direction:
+    UP = 1
+    DOWN = 2
+    LEFT = 3
+    RIGHT = 4
 
 class Position:
     def __init__(self, x, y):
@@ -19,34 +26,57 @@ class Position:
         return f"({self.x}, {self.y})"
     __repr__ = __str__
 
-class GameState:
-    def __init__(self, snake, direction, food, field_size):
-        self.snake = snake
-        self.direction = direction
-        self.food = food
-        self.field_size = field_size
 
-    def next_head(self, direction):
+INITIAL_SNAKE = [Position(1, 2), Position(2, 2), Position(3, 2)]
+INITIAL_DIRECTION = Direction.RIGHT
+
+class GameState:
+    def __init__(self, snake=None, direction=None, food=None, field_size=cubes_num):
+        self.field_size = field_size
+        self.snake = snake if snake else INITIAL_SNAKE[:]
+        self.direction = direction if direction else INITIAL_DIRECTION
+        self.food = food
+        self.initial_position()
+
+    def next_head_position(self, direction):
         pos = self.snake[-1]
         if direction == Direction.UP:
-            return(pos.x, pos.y - 1)
-        if direction == Direction.DOWN:
-            return(pos.x, pos.y + 1)
-        if direction == Direction.LEFT:
-            return(pos.x - 1, pos.y)
-        if direction == Direction.RIGHT:
-            return(pos.x, pos.y + 1)
-    def step(self):
-        new_head = self.next_head(self.direction)
+            return Position(pos.x, (pos.y - 1) % self.field_size)
+        elif direction == Direction.DOWN:
+            return Position(pos.x, (pos.y + 1) % self.field_size)
+        elif direction == Direction.RIGHT:
+            return Position((pos.x + 1) % self.field_size, pos.y)
+        elif direction == Direction.LEFT:
+            return Position((pos.x - 1) % self.field_size, pos.y)
+    def initial_position(self):
+        self.snake = INITIAL_SNAKE[:]
+        self.direction = INITIAL_DIRECTION
+        self.set_random_food_position()
+
+    def step_move_snake(self):
+        new_head = self.next_head_position(self.direction)
+        if new_head in self.snake:  # Collision with itself
+            self.initial_position()
+            return
         self.snake.append(new_head)
-        self.snake = self.snake[:-1]
+        if new_head == self.food:  # Eating food
+            self.set_random_food_position()
+        else:
+            self.snake.pop(0)
+    def set_random_food_position(self):
+        self.food = Position(randint(0, self.field_size - 1), randint(0, self.field_size - 1))
+        while True:
+            self.food = Position(randint(0, self.field_size - 1), randint(0, self.field_size - 1))
+            if self.food not in self.snake:
+                break
 
+    def turn(self, direction):
+        if self.can_turn(direction):
+            self.direction = direction
 
-class Direction:
-    UP = 1
-    DOWN = 2
-    LEFT = 3
-    RIGHT = 4
+    def can_turn(self, direction):
+        new_head = self.next_head_position(direction)
+        return new_head != self.snake[-2]
 
 def draw_snake(snake):
     for part in snake:
@@ -69,7 +99,27 @@ def draw(snake, food):
     draw_food(food)
     pygame.display.update()
 
+state = GameState()
+clock = pygame.time.Clock()
 while True:
+    clock.tick(10)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
+            quit()
+        elif event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_LEFT:
+                state.turn(Direction.LEFT)
+
+            elif event.key == pygame.K_RIGHT:
+                state.turn(Direction.RIGHT)
+
+            elif event.key == pygame.K_UP:
+                state.turn(Direction.UP)
+
+            elif event.key == pygame.K_DOWN:
+                state.turn(Direction.DOWN)
+
+    state.step_move_snake()
+    draw(state.snake, state.food)
